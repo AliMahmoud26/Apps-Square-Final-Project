ea7d13a22cc6f332228df7de5b28e904a3dac4c8
AliMahmoud26 <AliMahmoudHelal26@gmail.com>
2024-10-02 23:40:04 +0300

    updating my dashboard component

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,24 +1,9 @@
-// app/dashboard/page.jsx
-import { useAuth } from '../protected-routes/AuthProvider';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import React from 'react'
 
 const Dashboard = () => {
-  const { isAuthenticated } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      router.push('/protected-routes'); // Redirect to login if not authenticated
-    }
-  }, [isAuthenticated, router]);
-
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard!</p>
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default Dashboard;
+export default Dashboard
